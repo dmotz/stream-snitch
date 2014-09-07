215bf78022b752e826c8a15b7b9d71af81bd6a04
Dan Motzenbecker <dan@oxism.com>
2014-09-07 01:37:45 -0400

    added simple tests

---
+++ test/main.js
@@ -0,0 +1,40 @@
+var fs = require('fs'),
+    StreamSnitch = require('../');
+
+require('should');
+
+describe('stream-snitch', function() {
+
+  describe('#StreamSnitch()', function() {
+
+    it('should match all the image tags', function(done) {
+      var snitch     = new StreamSnitch(/<img.+src=["'](.+)['"].?>/gi),
+          fileStream = fs.createReadStream(__dirname + '/test.html'),
+          count      = 0;
+
+      snitch.on('match', function(match) { count++; });
+
+      fileStream.pipe(snitch);
+      fileStream.on('end', function() {
+        count.should.equal(9);
+        done();
+      });
+    });
+
+    it('should match all the image tags in multiline mode', function(done) {
+      var snitch     = new StreamSnitch(/<img.+src=["'](.+)['"].?>/gim),
+          fileStream = fs.createReadStream(__dirname + '/test.html'),
+          count      = 0;
+
+      snitch.on('match', function(match) { count++; });
+
+      fileStream.pipe(snitch);
+      fileStream.on('end', function() {
+        count.should.equal(9);
+        done();
+      });
+    });
+
+  });
+
+});
